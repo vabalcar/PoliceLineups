f97627a73d844b7594372fa19577dee018162398
vabalcar <vabalcar@gmail.com>
2020-01-20 19:37:47 +0100

    Start DB on run when necessary

--- run.ps1
+++ run.ps1
@@ -2,6 +2,7 @@
 $cmdWrapper = (Join-Path '..' 'common' 'pwsh' 'cmd-wrapper.ps1')
 
 [Executor]::ExecuteParallelly(@(
+    @{Script = 'run.ps1'; WD = 'db'; isExternal = $true; Wrapper = $cmdWrapper},
     @{Script = 'run.ps1'; WD = 'server'; isExternal = $true; Wrapper = $cmdWrapper},
     @{Script = 'run.ps1'; WD = 'client'; isExternal = $true; Wrapper = $cmdWrapper}
 ))
