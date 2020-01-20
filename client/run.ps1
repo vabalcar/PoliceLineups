e4507e7fba8e054cfb909e39982c018c6d127295
vabalcar <vabalcar@gmail.com>
2020-01-20 16:24:53 +0100

    Angular 8

--- client/run.ps1
+++ client/run.ps1
@@ -1,4 +1,4 @@
 $config = Get-Content (Join-Path '..' 'config' 'client.json') | ConvertFrom-Json
 Write-Host 'Running client...'
-& (Join-Path '..' 'common' 'pwsh' 'ctrlc-wrapper.ps1') -Proc (Start-Process -nnw -PassThru -Path 'ng' -Args 'serve', '--open', '--host', $config.host, '--port', $config.port)
+& (Join-Path '..' 'common' 'pwsh' 'ctrlc-wrapper.ps1') -Proc (Start-Process -NoNewWindow -PassThru -FilePath 'pwsh' -Args '-Command', "& {ng serve --open --host $($config.host) --port $($config.port)}")
 Write-Host 'stopped.'
