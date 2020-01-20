f0fb91529d9f71230bf9ffac95783719e74e2154
vabalcar <vabalcar@gmail.com>
2020-01-20 16:58:53 +0100

    Fixed waiting for pressing Ctrl+C

--- common/pwsh/ctrlc-wrapper.ps1
+++ common/pwsh/ctrlc-wrapper.ps1
@@ -1,17 +1,28 @@
+using namespace System
+
 Param (
     [Parameter(Mandatory = $true)] [System.Diagnostics.Process] $proc
 )
 
 [Console]::TreatControlCAsInput = $true
-do {
-    $Host.UI.RawUI.FlushInputBuffer()
-    $keyPress = $Host.UI.RawUI.ReadKey("AllowCtrlC,IncludeKeyUp,NoEcho")
-} while (!($keyPress.Character -eq "`u{0003}"))
-$Host.UI.RawUI.FlushInputBuffer()
-[Console]::TreatControlCAsInput = $false
 
-if ($IsWindows) {
-    & 'taskkill' '/t' '/f' '/PID' $proc.Id
-} else {
-    # TODO: Add POSIX alternative
+try {
+
+    do {
+        
+        $Host.UI.RawUI.FlushInputBuffer()
+        $keyPress = $Host.UI.RawUI.ReadKey("AllowCtrlC,IncludeKeyDown,NoEcho")
+
+    } while (!($keyPress.Character -eq "`u{0003}"))
+    
+    $Host.UI.RawUI.FlushInputBuffer()
+    [Console]::TreatControlCAsInput = $false
+
+} catch [NotImplementedException] {
+
+    [Console]::TreatControlCAsInput = $false
+    $proc.WaitForExit()
+
 }
+
+& taskkill /t /f /PID $proc.Id
