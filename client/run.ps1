278da2b399851a538fcba36bb5c4de6d557188bd
vabalcar <vabalcar@gmail.com>
2020-07-20 13:16:32 +0200

    Run and install on Linux without major problems

--- client/run.ps1
+++ client/run.ps1
@@ -1,4 +1,12 @@
-$config = Get-Content (Join-Path '..' 'config' 'client.json') | ConvertFrom-Json
+$apiConfig = Get-Content (Join-Path '..' 'api' 'api.json') | ConvertFrom-Json
+$clientConfig = Get-Content (Join-Path '..' 'config' 'client.json') | ConvertFrom-Json
+
+if ($clientConfig.outPort -eq 80) { $clientConfig.outPort = '' }
+else { $clientConfig.outPort = ":${$clientConfig.outPort}" }
+
 Write-Host 'Running client...'
-& (Join-Path '..' 'common' 'pwsh' 'ctrlc-wrapper.ps1') -Proc (Start-Process -NoNewWindow -PassThru -FilePath 'pwsh' -Args '-Command', "& {ng serve --open --host $($config.host) --port $($config.port)}")
+
+$publicHost = "$($apiConfig.schemes[0])://$($clientConfig.outHost)$($clientConfig.outPort)$($clientConfig.outBasePath)/"
+& ng serve --open --host $($clientConfig.host) --port $($clientConfig.port) --publicHost=$publicHost
+
 Write-Host 'stopped.'
