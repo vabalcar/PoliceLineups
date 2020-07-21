7745ac85df42da4bb8dd5ee6b1ecfc080d7181a3
vabalcar <vabalcar@gmail.com>
2020-07-21 13:52:01 +0200

    Default config in repo, current config out of repo

---
+++ config/install.ps1
@@ -0,0 +1,9 @@
+Get-ChildItem -Path 'default' -File -Filter '*.json' | ForEach-Object {
+    $newFileName = $_.Name
+    if (!(Test-Path -PathType Leaf -Path $newFileName)) {
+        "Creating $newFileName..." | Out-Host
+        Copy-Item -Path $_ -Destination $newFileName
+    } else {
+        "$newFileName already exists." | Out-Host
+    }
+}
