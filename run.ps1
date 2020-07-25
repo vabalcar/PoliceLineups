d0edaf9419f944f0764c0d693ad7c5facd35f50d
vabalcar <vabalcar@gmail.com>
2020-07-25 21:49:14 +0200

    Run DB non-externally

--- run.ps1
+++ run.ps1
@@ -3,7 +3,7 @@
 $cmdWrapper = (Join-Path '..' 'pwsh' 'libs' 'cmd-wrapper.ps1')
 
 [Executor]::ExecuteParallelly(@(
-    @{Script = 'run.ps1'; WD = 'db'; isExternal = $true; Wrapper = $cmdWrapper},
+    @{Script = 'run.ps1'; WD = 'db'},
     @{Script = 'run.ps1'; WD = 'server'; isExternal = $true; Wrapper = $cmdWrapper},
     @{Script = 'run.ps1'; WD = 'client'; isExternal = $true; Wrapper = $cmdWrapper}
 ))
