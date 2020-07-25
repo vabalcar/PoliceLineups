cb79ecfea1e0c21db6234b90d8f8f7bfc6023931
vabalcar <vabalcar@gmail.com>
2020-07-25 16:31:19 +0200

    Added script for Python venv activation

---
+++ server/activate.ps1
@@ -0,0 +1,12 @@
+#!/usr/bin/pwsh
+$packageInfo = Get-Content package-info.json | ConvertFrom-Json
+
+$venvName = $IsWindows ? $packageInfo.venvNameWindows : $packageInfo.venvName
+$activationScript = $IsWindows ? (Join-Path $venvName 'Scripts' 'Activate.ps1') : (Join-Path $venvName 'bin' 'Activate.ps1')
+
+if(!(Test-Path -PathType Leaf $activationScript)) {
+    "Creating virtual environment '$venvName'..." | Out-Host
+    & virtualenv $venvName
+}
+
+& $activationScript
