1558eb90023a1eb1b332dcd955d7c546e28171bc
vabalcar <vabalcar@gmail.com>
2020-07-25 17:49:26 +0200

    Server deps in requirements.txt

--- server/activate.ps1
+++ server/activate.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/pwsh
-$packageInfo = Get-Content package-info.json | ConvertFrom-Json
+$packageInfo = Get-Content 'virtualenv.json' | ConvertFrom-Json
 
 $venvName = $IsWindows ? $packageInfo.venvNameWindows : $packageInfo.venvName
 $activationScript = $IsWindows ? (Join-Path $venvName 'Scripts' 'Activate.ps1') : (Join-Path $venvName 'bin' 'Activate.ps1')
--- server/install.ps1
+++ server/install.ps1
@@ -1,13 +1,8 @@
 #!/usr/bin/pwsh
-$packageInfo = Get-Content package-info.json | ConvertFrom-Json
-
 & (Join-Path '.' 'activate.ps1')
 
 'Installing dependencies...' | Out-Host
-$deps = $packageInfo.dependencies
-foreach ($dep in $deps) {
-    & pip install $dep
-}
+& pip install -r requirements.txt
 'done.' | Out-Host
 
 & deactivate
