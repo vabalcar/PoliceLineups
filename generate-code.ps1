db3af0e08eea323407de01470c7e893254b52b5c
vabalcar <vabalcar@gmail.com>
2020-07-26 18:57:08 +0200

    Generate DB code in global generate-code task

--- generate-code.ps1
+++ generate-code.ps1
@@ -2,6 +2,7 @@
 . (Join-Path '.' 'pwsh' 'libs' 'script-executing.ps1')
 
 [Executor]::ExecuteParallelly(@(
+    @{Script = 'generate-code.ps1'; WD = 'db'},
     @{Script = 'generate-code.ps1'; WD = 'server'},
     @{Script = 'generate-code.ps1'; WD = 'client'}
 ))
