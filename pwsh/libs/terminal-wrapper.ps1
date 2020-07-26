8c1281d55ecf21497a2e33e2dd3f1854fe015561
vabalcar <vabalcar@gmail.com>
2020-07-26 15:03:01 +0200

    More PowerShell, less .NET Core

--- pwsh/libs/terminal-wrapper.ps1
+++ pwsh/libs/terminal-wrapper.ps1
@@ -13,14 +13,14 @@ try{
     }
 } finally {
     try {
-        [Console]::Write('Press any key to close this window...')
+        Write-Host -NoNewline 'Press any key to close this window...'
         $Host.UI.RawUI.Flushinputbuffer()
         $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown') | Out-Null
         $Host.UI.RawUI.Flushinputbuffer()
         Write-Host
     } catch [MethodInvocationException] {
         'error - waiting for any keypress is not supported on this platforms/terminal.' | Out-Host
-        [Console]::Write('Press enter to close this window...')
+        Write-Host -NoNewline 'Press enter to close this window...'
         Read-Host
     }
 }
