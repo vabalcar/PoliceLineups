276bfa4cd8a32269109d5a87c8c8c078cfbd9bb2
vabalcar <vabalcar@gmail.com>
2020-07-28 21:35:57 +0200

    Run DB on Linux

--- db/run.ps1
+++ db/run.ps1
@@ -3,15 +3,23 @@ param (
     [string] $MysqlService = $IsWindows ? 'MYSQL80' : 'mysql'
 )
 
-$dbStatus = (Get-Service $MysqlService).Status
-
-if ($dbStatus -ne 'Running') {
-    if ($IsWindows) {
-        Start-Process -Wait -Verb RunAs -FilePath 'pwsh' -Args '-NoLogo', '-Command', "Start-Service $MysqlService"
-    } else {
-        & sudo service $MysqlService start
-    }
+'Running db...' | Out-Host
+if ($IsWindows) {
     $dbStatus = (Get-Service $MysqlService).Status
-}
 
-"DB (service $MysqlService) is $($dbStatus.ToString().ToLower())" | Out-Host
+    if ($dbStatus -ne 'Running') {
+        Start-Process -Wait -Verb RunAs -FilePath 'pwsh' -Args '-NoLogo', '-Command', "Start-Service $MysqlService"
+        $dbStatus = (Get-Service $MysqlService).Status
+    }
+
+    "DB (service $MysqlService) is $($dbStatus.ToString().ToLower())" | Out-Host
+} else {
+    if (Get-Command -CommandType Application -TotalCount 1 -Name 'genie' -ErrorAction SilentlyContinue) {
+        # WSL 2 support
+        & genie -c sudo systemctl start $MysqlService
+        & genie -c sudo systemctl status $MysqlService
+    } else {
+        & sudo systemctl start $MysqlService
+        & sudo systemctl status $MysqlService
+    }
+}
