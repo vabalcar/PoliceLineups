d1eb1d8264fae1a2e64cdc4a8ca11b28438395c8
vabalcar <vabalcar@gmail.com>
2020-07-28 21:10:00 +0200

    Improved generation of code using swagger

--- api/generate-code.ps1
+++ api/generate-code.ps1
@@ -14,7 +14,7 @@ function Invoke-SwaggerCodegen {
         [Parameter(Mandatory = $true)] [string] $API
     )
 
-    "Generating $Language api in $Directory..." | Out-Host
+    "Generating $Language api in $Directory" | Out-Host
     $curDir = $PSScriptRoot
     $swaggerCli = Join-Path $curDir 'swagger-codegen-cli.jar'
     $swaggerInput = Join-Path $curDir $API
@@ -27,17 +27,27 @@ Invoke-SwaggerCodegen -Language $Language -Directory $TmpDirectory -API $API
 Get-ChildItem -Force -Path $TmpDirectory
     | Where-Object -Property Name -NotIn (Get-Content $APIGenerationWhitelist | ConvertFrom-Json)
     | ForEach-Object {
-        "Removing $_..." | Out-Host
+        "Removing $_" | Out-Host
         $_
     }
     | Remove-Item -Recurse -Force -ErrorAction Ignore
 
+if (!(Test-Path -PathType Container -Path $Directory)) {
+    New-Item -ItemType Directory -Path $Directory | Out-Null
+    "Created directory $Directory" | Out-Host
+}
+
 Get-ChildItem -Force -Path $Directory
     | Where-Object -Property Name -In (Get-Content $APIGenerationWhitelist | ConvertFrom-Json)
     | ForEach-Object {
-        "Removing $_..." | Out-Host
+        "Removing $_" | Out-Host
         $_
     }
     | Remove-Item -Recurse -Force -ErrorAction Ignore
-Move-Item -Force -Path (Join-Path $TmpDirectory '*') -Destination $Directory
+
+$tmpDirectoryContent = Join-Path $TmpDirectory '*'
+"Moving $tmpDirectoryContent into $Directory" | Out-Host
+Move-Item -Force -Path $tmpDirectoryContent -Destination $Directory
+
+"Removing $TmpDirectory" | Out-Host
 Remove-Item -Recurse -Force -Path $TmpDirectory -ErrorAction Ignore
