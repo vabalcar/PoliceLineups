6ee7fa3603b142a0c221f99e4dd62cd4639275ff
vabalcar <vabalcar@gmail.com>
2020-07-30 14:59:47 +0200

    server - query_db in mysql_utils

--- server/src/controllers/people.py
+++ server/src/controllers/people.py
@@ -1,37 +1,8 @@
 """
 Controller for working with people.
 """
-import json
-from os import path
-import mysql.connector
-
+from police_lineups.mysql_utils import query_db
 from swagger_server.models import Person
-
-def query_db(query, result_processor):
-
-    wd = path.dirname(__file__)
-    config_path = path.join(wd, '..', '..', '..', 'config', 'db.json')
-    with open(config_path) as config_json:
-        db_config = json.load(config_json)
-
-    db = mysql.connector.connect(
-        host=db_config['host'],
-        user=db_config['user'],
-        passwd=db_config['password'],
-        database=db_config['db'],
-        port=db_config['port']
-    )
-
-    db_cursor = db.cursor()
-    db_cursor.execute(query)
-    raw_result = db_cursor.fetchall()
-    result = []
-    for raw_row in raw_result:
-        result.append(result_processor(raw_row))
-
-    db.close()
-
-    return result
 
 def parse_person_from_row(row):
     """
--- server/src/controllers/users.py
+++ server/src/controllers/users.py
@@ -1,34 +1,8 @@
-import json
-from os import path
-import mysql.connector
-
+"""
+Controller for working with users.
+"""
+from police_lineups.mysql_utils import query_db
 from swagger_server.models.user import User
-
-def queryDB(query, resultProcessor):
-
-    wd = path.dirname(__file__)
-    config_path = path.join(wd, '..', '..', 'config', 'db.json')
-    with open(config_path) as config_json:
-        db_config = json.load(config_json)
-
-    db = mysql.connector.connect(
-        host=db_config['host'],
-        user=db_config['user'],
-        passwd=db_config['password'],
-        database=db_config['db'],
-        port=db_config['port']
-    )
-
-    db_cursor = db.cursor()
-    db_cursor.execute(query)
-    raw_result = db_cursor.fetchall()
-    result = []
-    for raw_row in raw_result:
-        result.append(resultProcessor(raw_row))
-
-    db.close()
-
-    return result
 
 def parse_user_from_row(row):
     return User(id=row[0], name=row[1], email=row[2])
@@ -42,7 +16,7 @@ def get_users():  # noqa: E501
 
     :rtype: List[User]
     """
-    return queryDB("SELECT * FROM users", parse_user_from_row)
+    return query_db("SELECT * FROM users", parse_user_from_row)
 
 def get_user(person_id):  # noqa: E501
     """Returns a user.
@@ -54,4 +28,4 @@ def get_user(person_id):  # noqa: E501
 
     :rtype: User
     """
-    return queryDB('SELECT * FROM users WHERE id={}'.format(person_id), parse_user_from_row)[0]
+    return query_db('SELECT * FROM users WHERE id={}'.format(person_id), parse_user_from_row)[0]
