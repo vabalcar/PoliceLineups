fd8a0dcc9fa1e60a72ddd103fcfe90bb7011c1e5
vabalcar <vabalcar@gmail.com>
2020-09-09 17:58:53 +0200

    Server - random session key & improved main

---
+++ server/src/police_lineups/mysql/db.py
@@ -0,0 +1,14 @@
+from swagger_server.models import User, Person
+from police_lineups.mysql.utils import MysqlDBConnector, MysqlDBTable
+
+_db_tables = {
+    'users': User,
+    'people': Person
+}
+
+def setup_db():
+
+    MysqlDBConnector().load_db_config('..', '..', 'config', 'db.json')
+
+    for table in _db_tables:
+        MysqlDBTable(table).set_entity_type(_db_tables[table])
