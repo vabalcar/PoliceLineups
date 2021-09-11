54e13f46ecec360773358f80406761295b013a39
vabalcar <vabalcar@gmail.com>
2021-09-11 14:22:27 +0200

    Enabled backward compatibility for swagger codegen CLI

--- api/generate-code.ps1
+++ api/generate-code.ps1
@@ -24,6 +24,13 @@ function Invoke-SwaggerCodegen {
     if ($AdditionalConfiguration) {
         $swaggerCliArgs += '-c', $AdditionalConfiguration
     }
+
+    <#
+    Following line of code is a workaround described at https://docs.oracle.com/javase/9/migrate/toc.htm#JSMIG-GUID-12F945EB-71D6-46AF-8C3D-D354FD0B1781
+    to enable runnning swagger CLI on newer Java JREs than JRE 8 which seems to be oficially supported since swagger CLI is build using JDK 8 (see https://github.com/swagger-api/swagger-codegen/blob/master/run-in-docker.sh).
+    #>
+    $env:_JAVA_OPTIONS = "--add-opens=java.base/java.util=ALL-UNNAMED"
+
     & java -jar $swaggerCli @swaggerCliArgs
 
     'done.' | Out-Host
@@ -32,12 +39,12 @@ function Invoke-SwaggerCodegen {
 Remove-Item -Recurse -Force -Path $TmpDirectory -ErrorAction Ignore
 Invoke-SwaggerCodegen -Language $Language -Directory $TmpDirectory -API $API
 Get-ChildItem -Force -Path $TmpDirectory
-    | Where-Object -Property Name -NotIn (Get-Content $APIGenerationWhitelist | ConvertFrom-Json)
-    | ForEach-Object {
-        "Removing $_" | Out-Host
-        $_
-    }
-    | Remove-Item -Recurse -Force -ErrorAction Ignore
+| Where-Object -Property Name -NotIn (Get-Content $APIGenerationWhitelist | ConvertFrom-Json)
+| ForEach-Object {
+    "Removing $_" | Out-Host
+    $_
+}
+| Remove-Item -Recurse -Force -ErrorAction Ignore
 
 if (!(Test-Path -PathType Container -Path $Directory)) {
     New-Item -ItemType Directory -Path $Directory | Out-Null
@@ -45,12 +52,12 @@ if (!(Test-Path -PathType Container -Path $Directory)) {
 }
 
 Get-ChildItem -Force -Path $Directory
-    | Where-Object -Property Name -In (Get-Content $APIGenerationWhitelist | ConvertFrom-Json)
-    | ForEach-Object {
-        "Removing $_" | Out-Host
-        $_
-    }
-    | Remove-Item -Recurse -Force -ErrorAction Ignore
+| Where-Object -Property Name -In (Get-Content $APIGenerationWhitelist | ConvertFrom-Json)
+| ForEach-Object {
+    "Removing $_" | Out-Host
+    $_
+}
+| Remove-Item -Recurse -Force -ErrorAction Ignore
 
 $tmpDirectoryContent = Join-Path $TmpDirectory '*'
 "Moving $tmpDirectoryContent into $Directory" | Out-Host
