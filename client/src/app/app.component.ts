56ffc8f4d0f08c830bd89acba25ec72f261b971f
vabalcar <vabalcar@gmail.com>
2021-09-14 18:03:45 +0200

    login/logout button

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { BASE_PATH } from "./api/variables";
 import { AuthService } from "./auth.service";
 
@@ -8,15 +9,23 @@ import { AuthService } from "./auth.service";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  title = "client";
   showLogout = false;
 
   constructor(
     @Inject(BASE_PATH) public backend: string,
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit() {
     this.auth.isLoggedIn$.subscribe((loggedIn) => (this.showLogout = loggedIn));
+  }
+
+  redirectToLogin() {
+    this.router.navigateByUrl("/login");
+  }
+
+  logout() {
+    this.auth.logout();
   }
 }
