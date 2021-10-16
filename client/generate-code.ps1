e39c0a946560e7c26d4559e043e8968fea9f3497
vabalcar <vabalcar@gmail.com>
2021-10-16 21:14:10 +0200

    Removed dependency on powershell-yaml

--- client/generate-code.ps1
+++ client/generate-code.ps1
@@ -1,5 +1,2 @@
 #!/usr/bin/pwsh
-Import-Module powershell-yaml
-
-$apiDir = Join-Path '..' 'api'
-& (Join-Path $apiDir 'generate-code.ps1') -Language 'typescript-angular' -Directory (Join-Path 'src' 'app' 'api') -AdditionalConfiguration 'codegen-config.json'
+& (Join-Path '..' 'api' 'generate-code.ps1') -Language 'typescript-angular' -Directory (Join-Path 'src' 'app' 'api') -AdditionalConfiguration 'codegen-config.json'
--- pwsh/install.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/pwsh
-param (
-    [string] $ModulesListFile = 'modules.txt'
-)
-
-Get-Content -Path  $ModulesListFile | ForEach-Object {
-    "Installing $_..." | Out-Host
-    Install-Module $_
-}
