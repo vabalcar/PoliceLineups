8d6d4e33f933a47196d14f00fa96878a7b371c61
vabalcar <vabalcar@gmail.com>
2021-10-24 19:45:16 +0200

    Proxy respects configuration of server and client

--- proxy/src/Proxy/Program.cs
+++ proxy/src/Proxy/Program.cs
@@ -1,5 +1,68 @@
+dynamic? ParseJsonFile(params string[] jsonFile)
+    => JsonSerializer.Deserialize<ExpandoObject>(File.ReadAllText(Path.Combine(jsonFile)));
+
+ClusterConfig CreateClusterConfig(string clusterId, string clusterAddress)
+    => new ClusterConfig
+    {
+        ClusterId = clusterId,
+        Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase)
+        {
+            {
+                $"{clusterId}-destination",
+                new DestinationConfig
+                {
+                    Address = clusterAddress
+                }
+            }
+        }
+    };
+
+var configurationDirectory = Path.Combine("..", "config", "debug");
+
+var clientConfiguration = ParseJsonFile(configurationDirectory, "client.json");
+var proxyConfiguration = ParseJsonFile(configurationDirectory, "proxy.json");
+var serverConfiguration = ParseJsonFile(configurationDirectory, "server.json");
+
+const string ClientClusterId = "Client";
+const string ServerClusterId = "Server";
+
+var clusters = new[]
+{
+    CreateClusterConfig(ClientClusterId, $"http://{clientConfiguration?.host}:{clientConfiguration?.port}"),
+    CreateClusterConfig(ServerClusterId, $"http://{serverConfiguration?.host}:{serverConfiguration?.port}"),
+};
+
+var routes = new[]
+{
+    new RouteConfig
+    {
+        RouteId = "ClientRoute",
+        ClusterId = ClientClusterId,
+        Match = new RouteMatch
+        {
+            Path = "{**catch-all}"
+        }
+    },
+    new RouteConfig
+    {
+        RouteId = "ServerRoute",
+        ClusterId = ServerClusterId,
+        Match = new RouteMatch
+        {
+            Path = "/api/{**catch-all}"
+        },
+        Transforms = new[]
+        {
+            new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "PathRemovePrefix", "/api" }
+            }
+        }
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddReverseProxy().LoadFromMemory(routes, clusters);
 
 var app = builder.Build();
 app.MapReverseProxy();
---
+++ proxy/src/Proxy/Usings.cs
@@ -0,0 +1,4 @@
+global using Microsoft.Extensions.Primitives;
+global using System.Dynamic;
+global using System.Text.Json;
+global using Yarp.ReverseProxy.Configuration;
