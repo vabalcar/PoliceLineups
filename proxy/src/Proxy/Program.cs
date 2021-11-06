343ce36f6ed272b06a1c8783c1a0748433a77796
vabalcar <vabalcar@gmail.com>
2021-11-06 15:13:45 +0100

    Load SSL certs from files defined in configuration

--- proxy/src/Proxy/Program.cs
+++ proxy/src/Proxy/Program.cs
@@ -1,7 +1,10 @@
-dynamic? ParseJsonFile(params string[] jsonFile)
-    => JsonSerializer.Deserialize<ExpandoObject>(File.ReadAllText(Path.Combine(jsonFile)));
+static JsonElement ParseJsonFile(params string[] jsonFile)
+    => JsonDocument.Parse(File.ReadAllText(Path.Combine(jsonFile))).RootElement;
 
-ClusterConfig CreateClusterConfig(string clusterId, string clusterAddress)
+static JsonElement? GetPropertyValue(JsonElement? jsonObject, string propertyName)
+    => (jsonObject?.TryGetProperty(propertyName, out var jsonObjectProperty) ?? false) ? jsonObjectProperty : null;
+
+static ClusterConfig CreateClusterConfig(string clusterId, string clusterAddress)
     => new ClusterConfig
     {
         ClusterId = clusterId,
@@ -17,13 +20,11 @@ ClusterConfig CreateClusterConfig(string clusterId, string clusterAddress)
         }
     };
 
-X509Certificate2? GetCertificateOrDefault(string? host, bool isDevelopmentEnvironment)
-{
-    var certificate = GetCertificateFromCertsDirectoryOrDefault(host);
-    return certificate ?? GetCertificateFromCertificateStoreOrDefault(host, isDevelopmentEnvironment);
-}
+static X509Certificate2? GetCertificateOrDefault(dynamic? certConfiguration, string? host, bool isDevelopmentEnvironment)
+    => GetCertificateFromFilesOrDefault(certConfiguration, host)
+    ?? GetCertificateFromCertificateStoreOrDefault(host, isDevelopmentEnvironment);
 
-X509Certificate2? GetCertificateFromCertificateStoreOrDefault(string? host, bool isDevelopmentEnvironment)
+static X509Certificate2? GetCertificateFromCertificateStoreOrDefault(string? host, bool isDevelopmentEnvironment)
 {
     if (!OperatingSystem.IsWindows() || string.IsNullOrEmpty(host))
         return default;
@@ -36,39 +37,65 @@ X509Certificate2? GetCertificateFromCertificateStoreOrDefault(string? host, bool
     return foundCertificates.FirstOrDefault();
 }
 
-X509Certificate2? GetCertificateFromCertsDirectoryOrDefault(string? host)
+static X509Certificate2? GetCertificateFromFilesOrDefault(JsonElement? certConfiguration, string? host)
 {
-    if (string.IsNullOrEmpty(host))
-        return default;
+    var (certFile, certKeyFile) = GetCertificateFilePaths(certConfiguration, host);
 
-    const string certsDirectory = ".ssl";
-    var certFile = Path.Combine(certsDirectory, $"{host}.crt");
-    var certKeyFile = Path.Combine(certsDirectory, $"{host}.key");
-
-    if (!File.Exists(certFile) || !File.Exists(certKeyFile))
+    if (certFile is null || !File.Exists(certFile)
+        || certKeyFile is null || !File.Exists(certKeyFile))
         return default;
 
     return new X509Certificate2(X509Certificate2.CreateFromPemFile(certFile, certKeyFile).Export(X509ContentType.Pkcs12));
 }
 
+static (string? certFile, string? certKeyFile) GetCertificateFilePaths(JsonElement? certConfiguration, string? host)
+{
+    string? certFile, certKeyFile;
+
+    var os = OperatingSystem.IsWindows() ? "windows" : "linux";
+    var certConfigurationForCurrentOS = GetPropertyValue(certConfiguration, os);
+    certFile = GetPropertyValue(certConfigurationForCurrentOS, "certFile")?.GetString();
+    certKeyFile = GetPropertyValue(certConfigurationForCurrentOS, "keyFile")?.GetString();
+
+    if (certFile is null || certKeyFile is null)
+    {
+        const string certsDirectory = ".ssl";
+        var isHostSet = host is not null;
+        certFile = isHostSet ? Path.Combine(certsDirectory, $"{host}.crt") : default;
+        certKeyFile = isHostSet ? Path.Combine(certsDirectory, $"{host}.key") : default;
+    }
+
+    return (certFile, certKeyFile);
+}
+
 var appBuilder = WebApplication.CreateBuilder(args);
 
-var environment = appBuilder.Environment.IsDevelopment() ? "debug" : "production";
+var isDevelopmentEnvironment = appBuilder.Environment.IsDevelopment();
+var environment = isDevelopmentEnvironment ? "debug" : "production";
 var configurationDirectory = Path.Combine("..", "config", environment);
 
+var certConfiguration = ParseJsonFile(configurationDirectory, "cert.json");
 var clientConfiguration = ParseJsonFile(configurationDirectory, "client.json");
 var proxyConfiguration = ParseJsonFile(configurationDirectory, "proxy.json");
 var serverConfiguration = ParseJsonFile(configurationDirectory, "server.json");
 
-var proxyHost = $"{proxyConfiguration?.host}";
+var clientHost = GetPropertyValue(clientConfiguration, "host")?.GetString();
+var clientPort = GetPropertyValue(clientConfiguration, "port")?.GetUInt16();
+
+var proxyHost = GetPropertyValue(proxyConfiguration, "host")?.GetString();
+var proxyHttpPort = GetPropertyValue(proxyConfiguration, "httpPort")?.GetUInt16();
+var proxyHttpsPort = GetPropertyValue(proxyConfiguration, "httpsPort")?.GetUInt16();
+
+var serverHost = GetPropertyValue(serverConfiguration, "host")?.GetString();
+var serverPort = GetPropertyValue(serverConfiguration, "port")?.GetUInt16();
 
 const string ClientClusterId = "Client";
 const string ServerClusterId = "Server";
 
 var clusters = new[]
 {
-    CreateClusterConfig(ClientClusterId, $"http://{clientConfiguration?.host}:{clientConfiguration?.port}"),
-    CreateClusterConfig(ServerClusterId, $"http://{serverConfiguration?.host}:{serverConfiguration?.port}"),
+    CreateClusterConfig(ClientClusterId, $"http://{clientHost}:{clientPort}"),
+    CreateClusterConfig(ServerClusterId, $"http://{serverHost}:{serverPort}"),
 };
 
 var routes = new[]
@@ -103,8 +130,8 @@ var routes = new[]
 appBuilder.Services.AddReverseProxy().LoadFromMemory(routes, clusters);
 
 appBuilder.WebHost.UseUrls(
-    $"http://{proxyHost}:{proxyConfiguration?.httpPort}",
-    $"https://{proxyHost}:{proxyConfiguration?.httpsPort}"
+    $"http://{proxyHost}:{proxyHttpPort}",
+    $"https://{proxyHost}:{proxyHttpsPort}"
 );
 
 appBuilder.WebHost.ConfigureKestrel(kestrelOptions =>
@@ -115,7 +142,7 @@ appBuilder.WebHost.ConfigureKestrel(kestrelOptions =>
     });
     kestrelOptions.ConfigureHttpsDefaults(httpsConfiguration =>
     {
-        httpsConfiguration.ServerCertificate = GetCertificateOrDefault(proxyHost, appBuilder.Environment.IsDevelopment());
+        httpsConfiguration.ServerCertificate = GetCertificateOrDefault(certConfiguration, proxyHost, isDevelopmentEnvironment);
         httpsConfiguration.SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13;
     });
 });
