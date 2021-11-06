e73d8b4f89a7db4982642095f7ec35a8c472cc33
vabalcar <vabalcar@gmail.com>
2021-11-06 15:22:07 +0100

    Removed unnecessary using

--- proxy/src/Proxy/Usings.cs
+++ proxy/src/Proxy/Usings.cs
@@ -1,6 +1,5 @@
 global using Microsoft.AspNetCore.Server.Kestrel.Core;
 global using Microsoft.Extensions.Primitives;
-global using System.Dynamic;
 global using System.Security.Authentication;
 global using System.Security.Cryptography.X509Certificates;
 global using System.Text.Json;
