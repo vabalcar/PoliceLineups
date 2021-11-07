5f31caec5820095b03e8a6346cb4193f1ad5b540
vabalcar <vabalcar@gmail.com>
2021-11-07 18:41:09 +0100

    Faster safe service stopping

--- stop.ps1
+++ stop.ps1
@@ -1,2 +1,7 @@
 #!/usr/bin/pwsh
-Start-Process -Wait -NoNewWindow -Path 'pwsh' -ArgumentList '-NoLogo', '-Command', ". (Join-Path '.' 'utils' 'script-executor.ps1'); [ServiceScriptExecutor]::new().TerminateAllExecutions()"
+'Stopping all scripts run as a service...' | Out-Host
+
+Start-Process -PassThru -NoNewWindow -Path 'pwsh' -ArgumentList '-NoLogo', '-Command', ". (Join-Path '.' 'utils' 'script-executor.ps1'); [ServiceScriptExecutor]::new().TerminateAllExecutions()"
+| Wait-Process
+
+'done.' | Out-Host
