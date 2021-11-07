410433641393462b4feff60f206cca3763527c82
vabalcar <vabalcar@gmail.com>
2021-11-07 20:15:53 +0100

    Fixed service stopping from linux interactive shell

--- stop.ps1
+++ stop.ps1
@@ -1,7 +1,13 @@
 #!/usr/bin/pwsh
 'Stopping all scripts run as a service...' | Out-Host
 
-Start-Process -PassThru -NoNewWindow -Path 'pwsh' -ArgumentList '-NoLogo', '-Command', ". (Join-Path '.' 'utils' 'script-executor.ps1'); [ServiceScriptExecutor]::new().TerminateAllExecutions()"
-| Wait-Process
+. (Join-Path '.' 'utils' 'script-executor.ps1')
+
+$executor = [ServiceScriptExecutor]::new()
+$executor.TerminateAllExecutions()
+
+if (!$IsWindows) {
+    & stty sane
+}
 
 'done.' | Out-Host
