cb199835cdbb9937bf9e66f945e87a2fecc39a65
vabalcar <vabalcar@gmail.com>
2021-11-12 23:02:51 +0100

    Added rebuild scripts

---
+++ rebuild.ps1
@@ -0,0 +1,15 @@
+#!/usr/bin/pwsh
+param (
+    [switch] $Debug
+)
+
+. (Join-Path 'utils' 'script-executor.ps1')
+
+$executor = [SequentialScriptExecutor]::new()
+
+$commonArgs = $Debug ? @('-Debug') : @()
+
+$executor.Execute(@(
+        @{Script = 'clean.ps1' },
+        @{Script = 'build.ps1'; ArgumentList = $commonArgs }
+    ))
