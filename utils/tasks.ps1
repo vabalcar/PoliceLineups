1029a87fbe2104367f452fae1ef3f291f9ead6cb
vabalcar <vabalcar@gmail.com>
2021-11-13 11:09:16 +0100

    Unified rebuild task

---
+++ utils/tasks.ps1
@@ -0,0 +1,16 @@
+. (Join-Path $PSScriptRoot 'script-executor.ps1')
+
+function Invoke-Rebuild {
+    param (
+        [switch] $Debug
+    )
+
+    $executor = [SequentialScriptExecutor]::new()
+
+    $commonArgs = $Debug ? @('-Debug') : @()
+
+    $executor.Execute(@(
+            @{Script = 'clean.ps1' },
+            @{Script = 'build.ps1'; ArgumentList = $commonArgs }
+        ))
+}
