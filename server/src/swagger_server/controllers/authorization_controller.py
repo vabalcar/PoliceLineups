5d26c46413136c795dd347334b31723e12ead3e4
vabalcar <vabalcar@gmail.com>
2021-11-16 18:54:34 +0100

    Moved actual server controllers to police_lineups module

--- server/src/swagger_server/controllers/authorization_controller.py
+++ server/src/swagger_server/controllers/authorization_controller.py
@@ -2,7 +2,7 @@
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
-from controllers.auth import authorize_admin_by_token, authorize_user_by_token
+from police_lineups.controllers.auth import authorize_admin_by_token, authorize_user_by_token
 
 
 def check_JwtAuthUser(token):
