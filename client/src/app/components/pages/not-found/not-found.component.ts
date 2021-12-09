ca0dcda1da756dafe7e0ae662006568262171aee
vabalcar <vabalcar@gmail.com>
2021-12-09 19:57:57 +0100

    More universal message in not found page

--- client/src/app/components/pages/not-found/not-found.component.ts
+++ client/src/app/components/pages/not-found/not-found.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { AppState } from "src/app/state/app.state";
 import { selectUrl } from "src/app/state/router/router.selectors";
 
@@ -12,6 +13,8 @@ export class NotFoundComponent {
   readonly url: Observable<string>;
 
   constructor(private store: Store<AppState>) {
-    this.url = this.store.select(selectUrl);
+    this.url = this.store
+      .select(selectUrl)
+      .pipe(map((url) => (url === "/not-found" ? undefined : url)));
   }
 }
