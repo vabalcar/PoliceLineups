bee2a8967b31d18d062f0664d095dd7cac0491bb
vabalcar <vabalcar@gmail.com>
2021-12-21 21:34:57 +0100

    Introduced queue of notifications

--- client/src/app/services/notifications.service.ts
+++ client/src/app/services/notifications.service.ts
@@ -1,17 +1,55 @@
 import { Injectable } from "@angular/core";
-import { MatSnackBar } from "@angular/material/snack-bar";
+import { MatSnackBar, MatSnackBarConfig } from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: "root",
 })
 export class NotificationsService {
-  constructor(private snackBar: MatSnackBar) {}
+  private static readonly notificationConfiguration: MatSnackBarConfig = {
+    horizontalPosition: "center",
+    verticalPosition: "bottom",
+    duration: 5 * 1000, // miliseconds
+  };
+  private static readonly notificationDismissButtonText = "OK";
+
+  private readonly messages: Array<string>;
+  private notificationsAreShowing: boolean;
+
+  constructor(private snackBar: MatSnackBar) {
+    this.messages = [];
+    this.notificationsAreShowing = false;
+  }
 
   showNotification(message: string) {
-    this.snackBar.open(message, "OK", {
-      horizontalPosition: "center",
-      verticalPosition: "bottom",
-      duration: 5 * 1000, // miliseconds
-    });
+    this.messages.push(message);
+
+    if (!this.notificationsAreShowing) {
+      this.startShowingNotifications();
+    }
+  }
+
+  private startShowingNotifications() {
+    this.notificationsAreShowing = true;
+    this.showNextNotification();
+  }
+
+  private showNextNotification(): void {
+    if (this.messages.length === 0) {
+      this.notificationsAreShowing = false;
+      return;
+    }
+
+    this.doShowNotification(this.messages.pop());
+  }
+
+  private doShowNotification(message: string): void {
+    this.snackBar
+      .open(
+        message,
+        NotificationsService.notificationDismissButtonText,
+        NotificationsService.notificationConfiguration
+      )
+      .afterDismissed()
+      .subscribe(() => this.showNextNotification());
   }
 }
