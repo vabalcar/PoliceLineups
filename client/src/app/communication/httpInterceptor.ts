49d73532a3e953645a8531d78e7ba1749b733648
vabalcar <vabalcar@gmail.com>
2022-01-01 03:25:01 +0100

    Improved HttpRequestInterceptor

--- client/src/app/communication/httpInterceptor.ts
+++ client/src/app/communication/httpInterceptor.ts
@@ -4,21 +4,33 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { Inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+
+import { BASE_PATH } from "../api/variables";
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+  private blobsBasePath: string;
+
+  constructor(@Inject(BASE_PATH) basePath: string) {
+    this.blobsBasePath = `${basePath}/blobs/`;
+  }
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    if (req.url.includes("/blobs/")) {
+    if (this.isBlobRequest(req)) {
       req = req.clone({
         responseType: "blob",
       });
     }
 
     return next.handle(req);
+  }
+
+  private isBlobRequest(req: HttpRequest<any>): boolean {
+    return req.url.startsWith(this.blobsBasePath);
   }
 }
