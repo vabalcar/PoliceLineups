81fece03771a1e514509ff861efa11d323b908c6
vabalcar <vabalcar@gmail.com>
2022-05-01 16:22:22 +0200

    Simplified fatal error handling of BE running in debug mode

--- server/src/app_debug_runner.py
+++ server/src/app_debug_runner.py
@@ -9,7 +9,11 @@ server_configuration = parse_json_file(
 
 WSGIRequestHandler.protocol_version = "HTTP/1.1"
 
-Server().current.run(
-    host=server_configuration.get('host'),
-    port=server_configuration.get('port'),
-    debug=True)
+try:
+    Server().current.run(
+        host=server_configuration.get('host'),
+        port=server_configuration.get('port'),
+        debug=True)
+
+except BaseException as err:  # pylint: disable=broad-except
+    print(f"App crashed: {err}")
